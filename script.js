4bf2b69f34eb4aa49b169b1c2514788fc2cb7d43
Shakthi Balan <s.shakthibalan@gmail.com>
2026-01-04 19:04:07 +0530

    Title Changes: Abdrajput & Co. and Project Commando

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Fitness Lifestyle Coach"],
+        strings: ["Project Commando"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
